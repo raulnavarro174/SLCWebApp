12d4ef30916d87ebdce95c4b51a1ed2a44db3b3e
raul.navarro <raul.navarro@basetis.com>
2021-09-16 15:53:09 +0200

    Dorsals afegits + nou mètide

--- src/main/java/com/slc/component/Constants.java
+++ src/main/java/com/slc/component/Constants.java
@@ -7,11 +7,13 @@ import com.slc.model.Metodes;
 
 public class Constants {
 	
-	public static final List<Metodes> listMetodes = Arrays.asList(new Metodes("gols"), new Metodes("assist"),new Metodes("xuts_porta"), new Metodes("xuts_fora"), new Metodes("perdues"),
-				new Metodes("recuperacions"), new Metodes("intercepcions"), new Metodes("gols_generats")
+	public static final List<Metodes> listMetodes = Arrays.asList(new Metodes("partits"), new Metodes("gols"),
+			new Metodes("assist"), new Metodes("xuts_porta"), new Metodes("xuts_fora"), new Metodes("perdues"),
+				new Metodes("recuperacions"), new Metodes("intercepcions"), new Metodes("gols_generats"), 
+				new Metodes("regats")
 			);
 	
-	public static final List<Integer> jugadors = Arrays.asList(10, 14, 18);
+	public static final List<Integer> jugadors = Arrays.asList(1, 5, 6, 8, 9, 10, 11, 14, 15, 19, 26);
 	
 	public static final int NUM_METODES = listMetodes.size();
 	public static final int NUM_JUGADORS = jugadors.size();
