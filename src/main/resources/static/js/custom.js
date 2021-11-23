02b223d913731c2f11af016e4b0b9cc167da6b4e
raul.navarro <raul.navarro@basetis.com>
2021-11-23 10:09:13 +0100

    GK and GK's actions in different color + submit action in 2 clicks instead of 3

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,2 +1,24 @@
-alert("Hola! Loguejat plz");
-document.write("Bon dia!");
+$('document').ready(function(){
+
+$('.metode_select').click(function(){
+	$('form#main_form').submit();
+});
+
+$('.dorsal_select:contains(15)').css({
+	"background-color" : "#808080",
+	"color": "#ffffff"
+});
+	
+$('.dorsal_select:contains(15), .metode_select:contains(gol_encaixat), .metode_select:contains(aturada)').css({
+	"background-color" : "#808080",
+	"color": "#ffffff"
+});
+
+$(".dorsal_select").filter(function() {
+    return $(this).text() === "1";
+}).css({
+	"background-color" : "#808080",
+	"color": "#ffffff"
+});
+
+});
